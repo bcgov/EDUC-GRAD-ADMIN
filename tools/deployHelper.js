c2ebc9d0c68e8a9e9299cd481f36da686dda7f73
HKOHLI <harshit.Kohli@gov.bc.ca>
2021-04-15 10:01:51 -0700

    deployhelper Changes

--- tools/deployHelper.js
+++ tools/deployHelper.js
@@ -29,33 +29,3 @@ def performUIDeploy(String hostRoute, String stageEnv, String projectEnv, String
       }
     }
 }
-
-def deployUIStage(String hostRoute, String stageEnv, String projectEnv, String repoName, String appName, String jobName, String tag, String sourceEnv, String targetEnvironment, String appDomain, String rawApiDcURLFrontend, String minReplicasFE, String maxReplicasFE, String minCPUFE, String maxCPUFE, String minMemFE, String maxMemFE, String minReplicasBE, String maxReplicasBE, String minCPUBE, String maxCPUBE, String minMemBE, String maxMemBE) {
-  openshift.withCluster() {
-   openshift.withProject(projectEnv) {
-
-     echo "Tagging Image ${repoName}-frontend-static:${jobName} with version ${tag}"
-     openshift.tag("${sourceEnv}/${repoName}-frontend-static:${tag}", "${repoName}-frontend-static:${tag}")
-
-     echo "Processing DeploymentConfig ${appName}-frontend-static..."
-     def dcFrontendStaticTemplate = openshift.process('-f',
-       "${rawApiDcURLFrontend}",
-       "REPO_NAME=${repoName}",
-       "JOB_NAME=${jobName}",
-       "NAMESPACE=${projectEnv}",
-       "APP_NAME=${appName}",
-       "HOST_ROUTE=${hostRoute}",
-       "TAG=${tag}",
-       "MIN_REPLICAS=${minReplicasFE}",
-       "MAX_REPLICAS=${maxReplicasFE}",
-       "MIN_CPU=${minCPUFE}",
-       "MAX_CPU=${maxCPUFE}",
-       "MIN_MEM=${minMemFE}",
-       "MAX_MEM=${maxMemFE}"
-     )
-
-     echo "Applying Deployment ${appName}-frontend-static..."
-     def dcFrontendStatic = openshift.apply(dcFrontendStaticTemplate).narrow('dc')
-   }
-  }
-}
