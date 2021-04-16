2f2b3907ffefb79386c8bf905b5b65dfd7813579
HKOHLI <harshit.Kohli@gov.bc.ca>
2021-04-16 16:33:46 -0700

    adding allowed origins

--- frontend/src/services/StudentService.js
+++ frontend/src/services/StudentService.js
@@ -6,13 +6,13 @@ var apiClient = axios.create({
 
 export default {
     getStudentByPen(pen, token) {
-        const headers = { Accept: 'application/json','Content-Type': 'application/json','Access-Control-Allow-Origin': '*','Authorization': 'Bearer '+ token }
+        const headers = { Accept: 'application/json','Content-Type': 'application/json','Access-Control-Allow-Origin': 'https://grad-admin-77c02f-tools.apps.silver.devops.gov.bc.ca','Authorization': 'Bearer '+ token }
         //console.log("TOKEN IN SERVICE getStudentByPen " + token);
         return apiClient.get('/api/v1/' + pen,{ headers });
     },
 
     getStudentsByAdvancedSearch(advancedSearchInput, token){
-        const headers = { Accept: 'application/json','Content-Type': 'application/json','Access-Control-Allow-Origin': '*','Authorization': 'Bearer '+ token }
+        const headers = { Accept: 'application/json','Content-Type': 'application/json','Access-Control-Allow-Origin': 'https://grad-admin-77c02f-tools.apps.silver.devops.gov.bc.ca/','Authorization': 'Bearer '+ token }
         //console.log("TOKEN IN SERVICE getStudentsByAdvancedSearch " + token);
         let queryString = ""
         for (var key in advancedSearchInput) {
