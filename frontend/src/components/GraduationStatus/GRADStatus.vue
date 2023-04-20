669233f5f70457565d6a36592104b12a6c2e4375
Michael Tang <michael.tang@gov.bc.ca>
2023-04-20 13:47:39 -0700

    GRADT-684

--- frontend/src/components/GraduationStatus/GRADStatus.vue
+++ frontend/src/components/GraduationStatus/GRADStatus.vue
@@ -1268,6 +1268,14 @@ export default {
       if (this.editedGradStatus.schoolAtGrad == "") {
         this.editedGradStatus.schoolAtGrad = null;
       }
+      if (
+        this.studentGradStatus.program == "1950" &&
+        this.editGradStatus.program != "1950"
+      ) {
+        this.editedGradStatus.adultStartDate = "";
+        this.studentGradStatus.adultStartDate = "";
+        console.log(this.editGradStatus.adultStartDate);
+      }
       StudentService.editGraduationStatus(id, this.editedGradStatus)
         .then((response) => {
           this.updateStatus = response.data;
