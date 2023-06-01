542733b9791e3b406c6b18be44861937a86a578d
suzalflueck <samflueck95@gmail.com>
2023-06-01 10:06:17 -0700

    adjust dev and localhost banner

--- frontend/src/components/EnvironmentBanner.vue
+++ frontend/src/components/EnvironmentBanner.vue
@@ -30,8 +30,7 @@ export default {
       //simple solution to display banner to UI that indicates what environment the user is in;
       //currently determined via browser URL since this is a simple visual aid for devs and testers
       if (location.host.includes(this.environments.local)) {
-        //this.environment = "local";
-        this.environment = "test";
+        this.environment = "local";
       } else if (location.host.includes(this.environments.dev)) {
         this.environment = "dev";
       } else if (location.host.includes(this.environments.test)) {
@@ -61,13 +60,15 @@ export default {
   margin-right: 40px;
 }
 #env-banner .local-env {
-  background-color: #ffffff52;
+  background-color: #c1c1c152;
   color: #000;
   text-align: left;
   width: fit-content;
 }
 #env-banner .dev-env {
-  background-color: #ffa6009d;
+  background-color: #ffa60080;
+  width: 100%;
+  float: none;
 }
 #env-banner .test-env {
   background-color: #ff4b00;
