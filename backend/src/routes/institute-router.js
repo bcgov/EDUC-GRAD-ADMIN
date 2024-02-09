3d98921883190215e123f55cc89c4dee8a3c02ac
Shaun Lum <shaun.lum@gov.bc.ca>
2024-02-09 09:03:56 -0800

    aded file

---
+++ backend/src/routes/institute-router.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+const express = require('express');
+const router = express.Router();
+const config = require('../config/index');
+const auth = require('../components/auth');
+const roles = require("../components/roles");
+const { errorResponse, getBackendToken, getData, postData, putData, deleteData} = require('../components/utils');
+const isValidUiTokenWithStaffRoles = auth.isValidUiTokenWithRoles('GRAD_SYSTEM_COORDINATOR', [roles.Admin.StaffInfoOfficer, roles.Admin.StaffAdministration, roles.Admin.StaffGradProgramBA]);
+
+//Program Routes
+router.get('/*', getInstituteAPI);
+
+async function getInstituteAPI(req, res) {
+  const token = getBackendToken(req);
+  try {
+    const url = `${config.get('server:instituteAPIURL')}/institute` + req.url;
+    console.log("URL " + url)
+    const data = await getData(token, url, req.session?.correlationID);
+    return res.status(200).json(data);
+  } catch (e) {
+    if(e.data.message){
+      return errorResponse(res, e.data.message, e.status);
+    } else {
+      return errorResponse(res);
+    }
+  }
+}
+
+module.exports = router;
