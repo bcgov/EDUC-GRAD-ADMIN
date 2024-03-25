a6fa548b1574d583f95836e7fa069dff2d92f536
Tang <Michael.Tang@gov.bc.ca>
2024-03-25 15:58:49 -0700

    fixed programs page

--- frontend/src/views/Programs.vue
+++ frontend/src/views/Programs.vue
@@ -1,35 +1,43 @@
 <template>
-  <v-container class="programs-all">
+  <div class="programs-all">
     <h1>Programs</h1>
 
     <v-card>
-      <v-card-title class="px-3">Program Management</v-card-title>
-      <v-card-subtitle class="px-3">
-        <v-tabs v-model="tab" background-color="transparent" color="blue">
+      <v-card-title> Program Management </v-card-title>
+      <v-card-subtitle>
+        <v-tabs v-model="tab">
           <v-tab to="/programs/algorithm-rules">Algorithm Rules</v-tab>
-          <v-tab-item-dropdown
-            text="Programs"
-            toggle-class="nav-link-programs"
-            left
-          >
-            <v-tab to="/programs/programs">Programs</v-tab>
-            <v-tab to="/programs/program-rules">Program Rules</v-tab>
-            <v-tab to="/programs/transcript-message"
-              >Transcript Messaging</v-tab
-            >
-          </v-tab-item-dropdown>
-
-          <v-tab-item-dropdown
-            text="Optional Programs"
-            toggle-class="nav-link-optional-programs"
-            left
-          >
-            <v-tab to="/programs/optional-programs">Optional Programs</v-tab>
-            <v-tab to="/programs/optional-program-rules"
-              >Optional Program Rules</v-tab
-            >
-          </v-tab-item-dropdown>
-
+          <v-tab>
+            <v-menu offset-y>
+              <template v-slot:activator="{ props }">
+                <v-btn text v-bind="props">Programs</v-btn>
+              </template>
+              <v-list>
+                <v-list-item to="/programs/programs">Programs</v-list-item>
+                <v-list-item to="/programs/program-rules"
+                  >Program Rules</v-list-item
+                >
+                <v-list-item to="/programs/transcript-message"
+                  >Transcript Messaging</v-list-item
+                >
+              </v-list>
+            </v-menu>
+          </v-tab>
+          <v-tab>
+            <v-menu offset-y>
+              <template v-slot:activator="{ props }">
+                <v-btn text v-bind="props">Optional Programs</v-btn>
+              </template>
+              <v-list>
+                <v-list-item to="/programs/optional-programs"
+                  >Optional Programs</v-list-item
+                >
+                <v-list-item to="/programs/optional-program-rules"
+                  >Optional Program Rules</v-list-item
+                >
+              </v-list>
+            </v-menu>
+          </v-tab>
           <v-tab to="/programs/letter-grades">Letter Grades</v-tab>
           <v-tab to="/programs/special-cases"
             >Assessment Special Case Codes</v-tab
@@ -38,10 +46,10 @@
         </v-tabs>
       </v-card-subtitle>
       <v-card-text>
-        <router-view v-bind:key="$route.fullPath"></router-view>
+        <router-view :key="$route.fullPath"></router-view>
       </v-card-text>
     </v-card>
-  </v-container>
+  </div>
 </template>
 
 <script>
@@ -60,13 +68,5 @@ export default {
 .programs-all {
   padding-left: 25px;
   padding-right: 25px;
-}
-
-.nav-link-programs {
-  color: blue;
-}
-
-.tab-item a {
-  border-radius: 0px !important;
 }
 </style>
