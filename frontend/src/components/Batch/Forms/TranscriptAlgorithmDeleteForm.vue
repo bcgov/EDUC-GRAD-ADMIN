db19af0dbc5138b02aeb43f53b6a57ae31ba9b3c
Shaun Lum <shaun.lum@gov.bc.ca>
2024-09-24 09:53:49 -0700

    adding missing file dependencies

---
+++ frontend/src/components/Batch/Forms/TranscriptAlgorithmDeleteForm.vue
@@ -0,0 +1,283 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent width="1024">
+      <template v-slot:activator="{ props }">
+        <v-btn small color="primary" v-bind="props"
+          ><v-icon>mdi-plus</v-icon></v-btn
+        >
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">GRADUATION ALGORITHM</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-stepper alt-labels show-actions v-model="step">
+              <template v-slot:default="{ prev, next }">
+                <v-stepper-header>
+                  <v-stepper-item
+                    :rules="[
+                      () =>
+                        !v$.getBatchRequest.hasAtLeastOneGroupValue.$invalid,
+                    ]"
+                    complete
+                    editable
+                    title="Group"
+                    value="1"
+                  ></v-stepper-item>
+
+                  <v-divider></v-divider>
+
+                  <v-stepper-item
+                    :rules="[
+                      () => !v$.getBatchRequest.batchRunTimeSet.$invalid,
+                    ]"
+                    complete
+                    editable
+                    title="Run/Schedule"
+                    value="2"
+                  ></v-stepper-item>
+                </v-stepper-header>
+
+                <v-stepper-window>
+                  <v-stepper-window-item value="1">
+                    <v-row>
+                      <v-select
+                        v-model="group"
+                        :items="['Student', 'School', 'All Students']"
+                        label="Select Option"
+                      ></v-select>
+                    </v-row>
+                    <v-row v-if="group == 'Student'">
+                      <StudentInput></StudentInput>
+                    </v-row>
+                    <v-row v-if="group == 'School Category'">
+                      <DistrictInput></DistrictInput>
+                    </v-row>
+                    <v-row v-if="group == 'Program'">
+                      <ProgramInput></ProgramInput>
+                    </v-row>
+                    <v-row v-if="group == 'School'">
+                      <SchoolInput></SchoolInput>
+                    </v-row>
+                  </v-stepper-window-item>
+
+                  <v-stepper-window-item value="2">
+                    <v-card title="Schedule" flat>
+                      <div v-if="group === 'School Category'">
+                        Districts:
+                        <v-list>
+                          <v-list-item
+                            v-for="(
+                              district, index
+                            ) in getBatchRequest.districts"
+                            :key="index"
+                          >
+                            <v-list-item-content>
+                              <v-list-item-title>{{
+                                district
+                              }}</v-list-item-title>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-list>
+                      </div>
+                      <div v-if="group === 'Program'">
+                        Districts: {{ getBatchRequest.programs }}
+                      </div>
+                      <div v-if="group === 'PSI'">
+                        Post Secondary Institutions: REQUEST
+                        {{ getBatchRequest }}
+                      </div>
+                      <v-btn @click="changeStep(0)">Edit</v-btn>
+
+                      <ScheduleInput></ScheduleInput>
+                    </v-card>
+                  </v-stepper-window-item>
+
+                  <v-stepper-window-item value="3">
+                    <span>Step Window 3</span>
+                  </v-stepper-window-item>
+                </v-stepper-window>
+                <v-stepper-actions
+                  @click:prev="prev"
+                  @click:next="next"
+                  @click:submit="submit"
+                ></v-stepper-actions>
+              </template>
+            </v-stepper>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue-darken-1" variant="text" @click="cancel">
+            Cancel
+          </v-btn>
+          <v-btn
+            :disabled="v$.$invalid"
+            color="blue-darken-1"
+            variant="text"
+            @click="submit"
+          >
+            Submit
+          </v-btn>
+        </v-card-actions>
+        STORE - GETBATCHREQUEST
+        {{ getBatchRequest }}
+
+        <br />
+        <br />
+        GRAD FORM VALIDATIONS V$
+        {{ v$.getBatchRequest }}
+        <br />
+        <br />
+        RUN SCHEDULE COMPONENT VALIDATIONS
+        {{ v$.RunLaterScheduleSet }}
+        <br />
+        <br />
+
+        ALL VAIDATIONS V$
+        {{ v$ }}
+        <br />
+        <br />
+        BATCH RUN TIME {{ batchRunTime }}
+        {{ v$.batchRunTime }}
+
+        <p v-for="error of v$.$errors" :key="error.$uid">
+          {{ error.$message }}
+        </p>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+import SchoolInput from "@/components/Batch/Forms/FormInputs/SchoolInput.vue";
+import DistrictInput from "@/components/Batch/Forms/FormInputs/DistrictInput.vue";
+import StudentInput from "@/components/Batch/Forms/FormInputs/StudentInput.vue";
+import ProgramInput from "@/components/Batch/Forms/FormInputs/ProgramInput.vue";
+import ScheduleInput from "@/components/Batch/Forms/FormInputs/ScheduleInput.vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, helpers } from "@vuelidate/validators";
+import { useBatchRequestFormStore } from "../../../store/modules/batchRequestFormStore";
+import { mapActions, mapState } from "pinia";
+export default {
+  setup() {
+    const batchRequestFormStore = useBatchRequestFormStore();
+    const group = ref(batchRequestFormStore.who);
+    watch(group, (newValue) => {
+      batchRequestFormStore.who = newValue;
+      if (newValue == "All Students") {
+        batchRequestFormStore.setActivityCode("All");
+      } else {
+        batchRequestFormStore.setActivityCode(null);
+      }
+    });
+
+    return {
+      group,
+      v$: useVuelidate(),
+    };
+  },
+  created() {},
+  validations() {
+    return {
+      getBatchRequest: {
+        batchRunTimeSet: helpers.withMessage("Runtime not set", (value) => {
+          if (this.getBatchRunTime) {
+            if (this.getBatchRunTime == "Run Now") {
+              return true;
+            } else if (this.getBatchRunTime == "Run Later") {
+              if (this.getBatchRequestCrontime) {
+                return true;
+              } else return false;
+            }
+          } else return false;
+        }),
+        hasAtLeastOneGroupValue: helpers.withMessage(
+          "Must contain at least one " + this.group,
+          (value) => {
+            if (this.getBatchRequest) {
+              let isValid = false;
+              if (
+                this.group &&
+                [
+                  "Student",
+                  "School",
+                  "School Category",
+                  "Program",
+                  "Psi",
+                ].includes(this.group)
+              ) {
+                if (this.group === "School") {
+                  isValid =
+                    this.getBatchRequest.schoolOfRecords &&
+                    this.getBatchRequest.schoolOfRecords.length > 0;
+                } else if (this.group === "Student") {
+                  console.log(this.getBatchRequest.students);
+                  isValid =
+                    this.getBatchRequest.pens &&
+                    this.getBatchRequest.pens.length > 0;
+                } else if (this.group === "School Category") {
+                  isValid =
+                    this.getBatchRequest.districts &&
+                    this.getBatchRequest.districts.length > 0;
+                } else if (this.group === "Program") {
+                  isValid =
+                    this.getBatchRequest.programs &&
+                    this.getBatchRequest.programs.length > 0;
+                } else if (this.group === "Psi") {
+                  isValid =
+                    this.getBatchRequest.psiCodes &&
+                    this.getBatchRequest.psiCodes.length > 0;
+                } else {
+                  isValid = true; // Return true if none of the above conditions matched
+                }
+                console.log(this.group);
+                console.log(isValid + " VALIDA");
+                return isValid;
+              }
+            } else {
+              return false;
+            }
+          }
+        ),
+      },
+    };
+  },
+  components: {
+    SchoolInput: SchoolInput,
+    DistrictInput: DistrictInput,
+    StudentInput: StudentInput,
+    ProgramInput: ProgramInput,
+    ScheduleInput: ScheduleInput,
+  },
+  data: () => ({
+    step: 0,
+    dialog: false,
+  }),
+  computed: {
+    ...mapState(useBatchRequestFormStore, [
+      "getBatchRequest",
+      "getBatchRunTime",
+    ]),
+  },
+  methods: {
+    ...mapActions(useBatchRequestFormStore, [
+      "clearBatchDetails",
+      "clearBatchGroupData",
+    ]),
+    cancel() {
+      this.group = null;
+      this.dialog = false;
+      this.clearBatchDetails();
+      console.log("CLEARING BATCH");
+      this.step = 0;
+    },
+    changeStep(step) {
+      this.step = step;
+    },
+  },
+};
+</script>
---
+++ frontend/src/store/modules/snackbar.js
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useSnackbarStore = defineStore('snackbar', () => {
+  const isVisible = ref(false);
+  const message = ref('');
+  const color = ref('success');
+  const timeout = ref(5000);
+
+  const showSnackbar = (msg, col = 'success', time = 5000) => {
+    console.log(msg)
+    message.value = msg;
+    color.value = col;
+    timeout.value = time;
+    isVisible.value = true;
+
+    setTimeout(() => {
+      isVisible.value = false;
+    }, timeout.value);
+  };
+
+  return { isVisible, message, color, timeout, showSnackbar };
+});
