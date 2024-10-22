e995a834241735e5e36be87f337bc54ff1023a9d
suzalflueck <samflueck95@gmail.com>
2024-10-22 08:38:01 -0700

    sort by updateDate working and optional program history ported over

--- frontend/src/components/StudentProfile/AuditHistory/StudentAuditHistory.vue
+++ frontend/src/components/StudentProfile/AuditHistory/StudentAuditHistory.vue
@@ -12,6 +12,7 @@
       <v-window v-model="selectedTab">
         <v-window-item value="studentChangeHistory">
           <v-data-table
+            :sortBy="sortBy"
             :items="studentHistory"
             :headers="studentChangeFields"
             :items-per-page="'-1'"
@@ -89,129 +90,66 @@
                 </div>
               </slot>
             </template> -->
-
-            <!-- <template v-slot:item.programCompletionDate="{ item }">
-              {{ item.programCompletionDate.value }}
-            </template>
-
-            <template v-slot:item.createDate="{ item }">
-              {{ item.createDate.value }}
-            </template>
-
-            <template v-slot:item.activityCode="{ item }">
-              {{ item.data.activityCodeDescription }}
-            </template>
-
-            <template v-slot:item.updateUser="{ item }">
-              {{ item.updateUser.value }}
-            </template>
-            <template v-slot:item.program="{ item }">
-              <div :class="item?.program.changed ? 'value-changed' : ''">
-                {{ item.program.value }}
-              </div>
-            </template>
-            <template v-slot:item.studentStatus="{ item }">
-              {{ item.studentStatus.value }}
-            </template>
-            <template v-slot:item.studentGrade="{ item }">
-              {{ item.studentGrade.value }}
-            </template>
-
-            <template v-slot:item.schoolOfRecord="{ item }">
-              {{ item.schoolOfRecord.value }}
-            </template>
-
-            <template v-slot:item.schoolAtGrad="{ item }">
-              {{ item.schoolAtGrad.value }}
-            </template>
-
-            <template v-slot:item.consumerEducationRequirementMet="{ item }">
-              {{ item.consumerEducationRequirementMet.value }}
-            </template>
-
-            <template v-slot:item.honoursStanding="{ item }">
-              {{ item.honoursStanding.value }}
-            </template>
-
-            <template v-slot:item.gpa="{ item }">
-              {{ item.gpa.value }}
-            </template>
-
-            <template v-slot:item.recalculateProjectedGrad="{ item }">
-              {{ item.recalculateProjectedGrad.value }}
-            </template>
-            <template v-slot:item.recalculateGradStatus="{ item }">
-              {{ item.recalculateGradStatus.value }}
-            </template>
-            <template v-slot:item.batchId="{ item }">
-              {{ item.batchId.value }}
-            </template> -->
           </v-data-table>
         </v-window-item>
 
         <v-window-item value="optionalProgramChangeHistory">
-          TODO: Optional Program Change History
-          <!-- <v-data-table
-            :items="optionalProgramChangeHighlight"
-            :fields="optionalProgramChangeFields"
-            showFilter="false"
-            title="Optional Program Change History"
-            :sort-desc="true"
-            :sortBy="'createDate'"
+          <v-data-table
+            :sortBy="sortBy"
+            :items="optionalProgramHistory"
+            :headers="optionalProgramChangeFields"
+            :items-per-page="'-1'"
+            item-value="historyId"
           >
-            <template #cell(more)="row">
-              <v-btn
-                variant="outlined"
-                size="sm"
-                @click="row.toggleDetails"
-                class="more-button"
-              >
-                <img
-                  v-show="!row.detailsShowing"
-                  src="../../../assets/images/icon-right.svg"
-                  width="9"
-                  aria-hidden="true"
-                  alt=""
-                />
-                <img
-                  v-show="row.detailsShowing"
-                  src="../../../assets/images/icon-down.svg"
-                  height="5"
-                  aria-hidden="true"
-                  alt=""
-                />
-              </v-btn>
+            <template v-slot:expanded-row="{ columns, item }">
+              <tr>
+                <td :colspan="columns.length">
+                  <div class="mx-5 my-5">
+                    <p>
+                      Changed By <strong>{{ item.updateUser }}</strong> on
+                      <strong>{{
+                        $filters.formatTime(item.updateDate)
+                      }}</strong>
+                    </p>
+                    <pre>
+                      {{ JSON.stringify(item, null, "\t") }}
+                    </pre>
+                  </div>
+                </td>
+              </tr>
             </template>
-
-            <template #row-details="row">
-              <v-card class="px-0 mt-0">
-                <p>
-                  <strong
-                    >Changed By {{ row.item.data.updateUser }} on
-                    {{ $filters.formatTime(row.item.data.updateDate) }}</strong
-                  >
-                </p>
-                <pre>
-                    {{ JSON.stringify(row.item.data, null, "\t") }}
-                  </pre
+            <template
+              v-slot:item.data-table-expand="{
+                item,
+                internalItem,
+                toggleExpand,
+                isExpanded,
+              }"
+            >
+              <td v-if="!!item">
+                <v-btn
+                  variant="text"
+                  density="comfortable"
+                  @click="toggleExpand(internalItem)"
+                  class="v-data-table__expand-icon"
+                  :class="{ 'v-data-table__expand-icon--active': isExpanded }"
+                  :icon="
+                    isExpanded(internalItem)
+                      ? 'mdi-chevron-down'
+                      : 'mdi-chevron-right'
+                  "
                 >
-              </v-card>
-            </template>
-
-            <template v-slot:item.createDate="{ item }">
-              {{ $filters.formatTime(item.createDate.value) }}
+                </v-btn>
+              </td>
             </template>
             <template v-slot:item.activityCode="{ item }">
-              {{ item.data.activityCodeDescription }}
+              <i>{{ item.activityCode }}</i> -
+              {{ item.activityCodeDescription }}
             </template>
-
-            <template v-slot:item="{ item }">
-              {{ item }}
-              <div :class="row.value.changed ? 'value-changed' : ''">
-                {{ item.row.value }}
-              </div>
+            <template v-slot:item.updateDate="{ item }">
+              {{ $filters.formatTime(item.updateDate) }}
             </template>
-          </v-data-table> -->
+          </v-data-table>
         </v-window-item>
       </v-window>
     </v-card-text>
@@ -256,148 +194,109 @@ export default {
       sortDesc: true,
       smallScreen: false,
       window: { width: 0, height: 0 },
+      sortBy: [{ key: "updateDate", order: "desc" }],
       studentChangeFields: [
-        //{ text: "Index", value: "index", sortable: false }, // Added index column
         {
           key: "data-table-expand",
           title: "",
-          sortable: true,
         },
         {
           key: "updateDate",
           title: "Date",
-          sortable: true,
-          sortDirection: "desc",
+          width: "115px",
         },
         {
           key: "activityCode",
           title: "Change",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "updateUser",
           title: "Update User",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "program",
           title: "Program",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "programCompletionDate",
           title: "Program Completion Date",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "studentStatus",
           title: "Status",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "studentGrade",
           title: "Grade",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "schoolOfRecord",
           title: "School of Record",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "schoolAtGrad",
           title: "School at Graduation",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "consumerEducationRequirementMet",
           title: "Consumer Ed",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "honoursStanding",
           title: "Honours",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "gpa",
           title: "GPA",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "recalculateProjectedGrad",
           title: "Recalc Projected Grad",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "recalculateGradStatus",
           title: "Recalc Grad",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "batchId",
           title: "Batch ID",
-          sortable: true,
-          sortDirection: "asc",
         },
       ],
       studentChangeHighlight: [],
       optionalProgramChangeFields: [
         {
-          key: "more",
+          key: "data-table-expand",
           title: "",
-          sortable: true,
         },
         {
-          key: "createDate",
+          key: "updateDate",
           title: "Date",
-          sortable: true,
-          sortDirection: "desc",
+          width: "115px",
         },
         {
           key: "activityCode",
           title: "Change",
-          sortable: true,
         },
         {
           key: "updateUser",
           title: "Update User",
           sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "programCode",
           title: "Program Code",
-          sortable: true,
         },
         {
           key: "optionalProgramCode",
           title: "Optional Program Code",
-          sortable: true,
-          sortDirection: "asc",
         },
         {
           key: "optionalProgramName",
           title: "Optional Program Name",
-          sortable: true,
         },
         {
           key: "optionalProgramCompletionDate",
           title: "Program Completion Date",
-          sortable: true,
         },
       ],
       optionalProgramChangeHighlight: [],
@@ -408,8 +307,8 @@ export default {
     };
   },
   mounted() {
-    this.highlightStudentHistoryChanges();
-    this.highlightOptionalProgramHistoryChanges();
+    // this.highlightStudentHistoryChanges();
+    // this.highlightOptionalProgramHistoryChanges();
   },
   created() {
     window.addEventListener("resize", this.handleResize);
