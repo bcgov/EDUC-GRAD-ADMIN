c5ce536d175ce4ff35cebd78ef4ed49ddce4f24a
cditcher <chris.ditcher@gov.bc.ca>
2024-12-03 15:13:56 -0800

    Updated update-configmap-backend.sh

--- tools/openshift/update-configmap-backend.sh
+++ tools/openshift/update-configmap-backend.sh
@@ -13,7 +13,82 @@ UI_PUBLIC_KEY=$9
 UI_PRIVATE_KEY=${10}
 REDIS_PASSWORD=${11}
 SPLUNK_TOKEN=${12}
+COMMON_NAMESPACE=${13}
 
+SOAM_KC_REALM_ID="master"
+SOAM_KC=soam-$ENV.apps.silver.devops.gov.bc.ca
+SOAM_KC_LOAD_USER_ADMIN=$(oc -n $COMMON_NAMESPACE-$ENV -o json get secret sso-admin-${ENV} | sed -n 's/.*"username": "\(.*\)"/\1/p' | base64 --decode)
+SOAM_KC_LOAD_USER_PASS=$(oc -n $COMMON_NAMESPACE-$ENV -o json get secret sso-admin-${ENV} | sed -n 's/.*"password": "\(.*\)",/\1/p' | base64 --decode)
+
+echo Fetching SOAM token
+TKN=$(curl -s \
+  -d "client_id=admin-cli" \
+  -d "username=$SOAM_KC_LOAD_USER_ADMIN" \
+  -d "password=$SOAM_KC_LOAD_USER_PASS" \
+  -d "grant_type=password" \
+  "https://$SOAM_KC/auth/realms/$SOAM_KC_REALM_ID/protocol/openid-connect/token" | jq -r '.access_token')
+
+echo Fetching public key from SOAM
+fullKey=$(curl -sX GET "https://$SOAM_KC/auth/admin/realms/$SOAM_KC_REALM_ID/keys" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TKN" \
+  | jq -r '.keys | .[] | select(.algorithm == "RS256") | .publicKey')
+
+echo Fetching public key from SOAM
+soamFullPublicKey="-----BEGIN PUBLIC KEY----- $fullKey -----END PUBLIC KEY-----"
+newline=$'\n'
+formattedPublicKey="${soamFullPublicKey:0:26}${newline}${soamFullPublicKey:27:64}${newline}${soamFullPublicKey:91:64}${newline}${soamFullPublicKey:155:64}${newline}${soamFullPublicKey:219:64}${newline}${soamFullPublicKey:283:64}${newline}${soamFullPublicKey:347:64}${newline}${soamFullPublicKey:411:9}${newline}${soamFullPublicKey:420}"
+
+echo Generating private and public keys
+ssh-keygen -b 4096 -t rsa -f tempPenBackendkey -m pem -q -N ""
+UI_PRIVATE_KEY_VAL="$(cat tempPenBackendkey)"
+UI_PUBLIC_KEY_VAL="$(ssh-keygen -f tempPenBackendkey -e -m pem)"
+echo Removing key files
+rm tempPenBackendkey
+rm tempPenBackendkey.pub
+
+
+###########################################################
+#Setup for config-maps
+###########################################################
+#### backend configmap
+echo Creating config map "$APP_NAME"-backend-config-map
+oc create -n "$OPENSHIFT_NAMESPACE" configmap "$APP_NAME"-backend-config-map \
+  --from-literal=NODE_ENV=openshift \
+  --from-literal=LOG_LEVEL=info \
+  --from-literal=SERVER_FRONTEND="https://$BASE_URL" \
+  --from-literal=SOAM_PUBLIC_KEY="$formattedPublicKey" \
+  --from-literal=SOAM_CLIENT_ID="$SOAM_CLIENT_ID" \
+  --from-literal=SOAM_CLIENT_SECRET="$SOAM_CLIENT_SECRET" \
+  --from-literal=SOAM_URL="https://soam-$ENV.apps.silver.devops.gov.bc.ca" \
+  --from-literal=SOAM_DISCOVERY="https://soam-$ENV.apps.silver.devops.gov.bc.ca/auth/realms/master/.well-known/openid-configuration" \
+  --from-literal=IDIR_IDP_HINT=keycloak_bcdevexchange_idir \
+  --from-literal=SITEMINDER_LOGOUT_ENDPOINT="$SITEMINDER_LOGOUT_ENDPOINT" \
+  --from-literal=ISSUER=GRAD_ADMIN_APPLICATION \
+  --from-literal=SESSION_MAX_AGE='1800000' \
+  --from-literal=TOKEN_EXPIRES_IN='1800000' \
+  --from-literal=UI_PUBLIC_KEY="$UI_PUBLIC_KEY_VAL" \
+  --from-literal=UI_PRIVATE_KEY="$UI_PRIVATE_KEY_VAL" \
+  --from-literal=GRAD_ROLE_ADMIN=GRAD_SYSTEM_COORDINATOR \
+  --from-literal=GRAD_PROGRAM_AREA_BA=GRAD_PROGRAM_AREA_BA \
+  --from-literal=GRAD_ROLE_INFO_OFFICER=GRAD_INFO_OFFICER \
+  --from-literal=REDIS_HOST=redis \
+  --from-literal=REDIS_PORT=6379 \
+  --from-literal=REDIS_PASSWORD="$REDIS_PASSWORD" \
+  --from-literal=BATCH_API_URL="http://educ-grad-batch-graduation-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=STUDENT_GRADUATION_API_URL="http://educ-grad-student-graduation-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=GRADUATION_API_URL="http://educ-grad-graduation-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=COURSE_API_URL="http://educ-grad-course-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=GRAD_STUDENT_API_URL="http://educ-grad-student-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=PROGRAM_API_URL="http://educ-grad-program-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=DISTRIBUTION_API_URL="http://educ-grad-distribution-api.e8a97a-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=ASSESSMENT_API_URL="http://educ-grad-assessment-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=GRADUATION_REPORT_API_URL="http://educ-grad-graduation-report-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=GRAD_REPORT_API_URL="http://educ-grad-report-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=GRAD_TRAX_API_URL="http://educ-grad-trax-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
+  --dry-run=client -o yaml | oc apply -f -
+
+#### splunk
 SPLUNK_URL="gww.splunk.educ.gov.bc.ca"
 FLB_CONFIG="[SERVICE]
    Flush        1
@@ -52,45 +127,6 @@ PARSER_CONFIG="
     Name        docker
     Format      json
 "
-###########################################################
-#Setup for config-maps
-###########################################################
-echo Creating config map "$APP_NAME"-backend-config-map
-oc create -n "$OPENSHIFT_NAMESPACE" configmap "$APP_NAME"-backend-config-map \
-  --from-literal=NODE_ENV=openshift \
-  --from-literal=LOG_LEVEL=info \
-  --from-literal=SERVER_FRONTEND="https://$BASE_URL" \
-  --from-literal=SOAM_PUBLIC_KEY="$SOAM_PUBLIC_KEY" \
-  --from-literal=SOAM_CLIENT_ID="$SOAM_CLIENT_ID" \
-  --from-literal=SOAM_CLIENT_SECRET="$SOAM_CLIENT_SECRET" \
-  --from-literal=SOAM_URL="https://soam-$ENV.apps.silver.devops.gov.bc.ca" \
-  --from-literal=SOAM_DISCOVERY="https://soam-$ENV.apps.silver.devops.gov.bc.ca/auth/realms/master/.well-known/openid-configuration" \
-  --from-literal=IDIR_IDP_HINT=keycloak_bcdevexchange_idir \
-  --from-literal=SITEMINDER_LOGOUT_ENDPOINT="$SITEMINDER_LOGOUT_ENDPOINT" \
-  --from-literal=ISSUER=GRAD_ADMIN_APPLICATION \
-  --from-literal=SESSION_MAX_AGE='1800000' \
-  --from-literal=TOKEN_EXPIRES_IN='1800000' \
-  --from-literal=UI_PUBLIC_KEY="$UI_PUBLIC_KEY" \
-  --from-literal=UI_PRIVATE_KEY="$UI_PRIVATE_KEY" \
-  --from-literal=GRAD_ROLE_ADMIN=GRAD_SYSTEM_COORDINATOR \
-  --from-literal=GRAD_PROGRAM_AREA_BA=GRAD_PROGRAM_AREA_BA \
-  --from-literal=GRAD_ROLE_INFO_OFFICER=GRAD_INFO_OFFICER \
-  --from-literal=REDIS_HOST=redis \
-  --from-literal=REDIS_PORT=6379 \
-  --from-literal=REDIS_PASSWORD="$REDIS_PASSWORD" \
-  --from-literal=BATCH_API_URL="http://educ-grad-batch-graduation-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=STUDENT_GRADUATION_API_URL="http://educ-grad-student-graduation-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=GRADUATION_API_URL="http://educ-grad-graduation-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=COURSE_API_URL="http://educ-grad-course-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=GRAD_STUDENT_API_URL="http://educ-grad-student-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=PROGRAM_API_URL="http://educ-grad-program-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=DISTRIBUTION_API_URL="http://educ-grad-distribution-api.e8a97a-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=ASSESSMENT_API_URL="http://educ-grad-assessment-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=GRADUATION_REPORT_API_URL="http://educ-grad-graduation-report-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=GRAD_REPORT_API_URL="http://educ-grad-report-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=GRAD_TRAX_API_URL="http://educ-grad-trax-api.77c02f-$ENV.svc.cluster.local:8080/api/v1" \
-  --dry-run=client -o yaml | oc apply -f -
-
 echo Creating config map "$APP_NAME"-flb-sc-config-map
 oc create -n "$OPENSHIFT_NAMESPACE" configmap "$APP_NAME"-flb-sc-config-map \
   --from-literal=fluent-bit.conf="$FLB_CONFIG" \
