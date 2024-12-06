73bb3e6a83398621a0678cfb53dd7f01865d4463
Tang <Michael.Tang@gov.bc.ca>
2024-12-06 14:25:09 -0800

    added v2 trax to branch

--- tools/openshift/update-configmap-backend.sh
+++ tools/openshift/update-configmap-backend.sh
@@ -96,6 +96,7 @@ oc create -n "$OPENSHIFT_NAMESPACE" configmap "$APP_NAME"-backend-config-map \
   --from-literal=GRADUATION_REPORT_API_URL="http://educ-grad-graduation-report-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080/api/v1" \
   --from-literal=GRAD_REPORT_API_URL="http://educ-grad-report-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080/api/v1" \
   --from-literal=GRAD_TRAX_API_URL="http://educ-grad-trax-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080/api/v1" \
+  --from-literal=GRAD_TRAX_API_URL_V2="http://educ-grad-trax-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080/api/v2" \
   --dry-run=client -o yaml | oc apply -f -
 
 #### splunk
