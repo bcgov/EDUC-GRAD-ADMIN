7446b7b7679e29117ca479b607e8a54e7ef287bf
KentoHyono <keeeeento4649@icloud.com>
2025-01-23 15:49:37 -0800

    Separated Mincode and District Code search for school reports to different tabs

--- frontend/src/views/SchoolReports.vue
+++ frontend/src/views/SchoolReports.vue
@@ -1,302 +1,50 @@
 <template>
   <div class="reports-view">
     <h1>School Reports</h1>
-
-    <v-card no-body>
-      <v-card-text>
-        <v-form v-on:submit.prevent id="psiReqForm">
-          <div class="advanced-search-form">
-            <div class="row mt-3">
-              <div class="advanced-search-field col-12 col-md-5">
-                <v-autocomplete
-                  id="mincode"
-                  label="Mincode"
-                  v-model="searchSchoolId"
-                  item-value="schoolId"
-                  :items="getSchoolsList"
-                  :item-title="schoolTitle"
-                  density="compact"
-                  variant="outlined"
-                >
-                </v-autocomplete>
-              </div>
-            </div>
-            <div class="row ml-1">
-              <div class="advanced-search-button">
-                <v-btn
-                  prepend-icon="mdi-magnify"
-                  v-on:click="schoolReportSearch"
-                  :loading="searchLoading"
-                  :disabled="searchLoading"
-                  variant="flat"
-                  color="primary"
-                  class="text-none"
-                  >Search</v-btn
-                >
-                <v-btn
-                  @click="clearInput"
-                  variant="outlined"
-                  color="primary"
-                  class="text-none mx-2"
-                >
-                  Reset
-                </v-btn>
-              </div>
-            </div>
-
-            <div v-if="reports">
-              <v-alert
-                type="success"
-                variant="tonal"
-                border="start"
-                class="mt-6 mb-0 ml-1 py-3 width-fit-content"
-                v-if="totalResults > 0 && !searchLoading"
-                :text="`${totalResults} report${
-                  totalResults > 1 ? 's' : ''
-                } found`"
-              >
-              </v-alert>
-              <v-alert
-                type="error"
-                variant="tonal"
-                border="start"
-                class="mt-6 mb-0 ml-1 py-3 width-fit-content"
-                v-if="!!searchMessage"
-                :text="searchMessage"
-              ></v-alert>
-            </div>
-          </div>
-        </v-form>
-
-        <DisplayTable
-          v-if="reports.length"
-          class="mt-12"
-          title="Results"
-          v-bind:items="reports"
-          v-bind:fields="reportFields"
-          sortKey="report"
-          id="mincode"
-          v-bind:showFilter="true"
-          pagination="true"
-        >
-          <template v-slot:item.report="{ item }">
-            <a
-              @click="
-                downloadFile(
-                  item.report,
-                  'application/pdf',
-                  'school-report_' +
-                    item.reportTypeCode +
-                    '_' +
-                    item.schoolOfRecord
-                )
-              "
-              href="#"
-              class="pdf-link float-left mt-2"
-            >
-              {{ item.reportTypeLabel }} (PDF)
-            </a>
-          </template>
-          <template v-slot:item.updateDate="{ item }">
-            {{ $filters.formatTime(item.updateDate) }}
-          </template>
-        </DisplayTable>
-      </v-card-text>
-    </v-card>
+    <div>
+      <v-card no-body>
+        <v-tabs v-model="tab" bg-color="bcGovLightGrey">
+          <v-tab value="schoolReportWithMincodeTab" class="text-none" size="large">
+            Mincode
+          </v-tab>
+          <v-tab value="schoolReportWithDistTab" class="text-none" size="large">
+            District Code
+          </v-tab>
+        </v-tabs>
+        <v-card-text>
+          <v-window v-model="tab">
+            <v-window-item value="schoolReportWithMincodeTab">
+              <SchoolReportsWithMincode />
+            </v-window-item>
+            <v-window-item value="schoolReportWithDistTab">
+              <SchoolReportsWithDist />
+            </v-window-item>
+          </v-window>
+        </v-card-text>
+      </v-card>
+    </div>
   </div>
 </template>
 
 <script>
-import DisplayTable from "@/components/DisplayTable.vue";
-import GraduationReportService from "@/services/GraduationReportService.js";
-import sharedMethods from "../sharedMethods";
-import { useSnackbarStore } from "@/store/modules/snackbar";
-import { mapState } from "pinia";
-import { useAppStore } from "../store/modules/app";
+import SchoolReportsWithDist from "../components/SchoolReports/SchoolReportsWithDist.vue";
+import SchoolReportsWithMincode from "../components/SchoolReports/SchoolReportsWithMincode.vue";
 
 export default {
   name: "SchoolReports",
   components: {
-    DisplayTable: DisplayTable,
+    SchoolReportsWithMincode: SchoolReportsWithMincode,
+    SchoolReportsWithDist: SchoolReportsWithDist,
   },
   data() {
     return {
-      snackbarStore: useSnackbarStore(),
-      url: null,
-      file: [],
-      mincode: "",
-      searchSchoolId: "",
-      totalResults: 0,
-      searchMessage: "",
-      searchLoading: false,
-      reports: {},
-      reportFields: [
-        {
-          key: "report",
-          title: "Report",
-          sortable: true,
-          sortDirection: "asc",
-        },
-        {
-          key: "reportTypeCode",
-          title: "Report Type Code",
-          sortable: true,
-          sortDirection: "asc",
-        },
-        {
-          key: "schoolOfRecord",
-          title: "Mincode",
-          sortable: true,
-          sortDirection: "asc",
-        },
-        {
-          key: "schoolOfRecordName",
-          title: "School Name",
-          sortable: true,
-          sortDirection: "asc",
-        },
-        {
-          key: "updateUser",
-          title: "Last Update User",
-          sortable: true,
-          sortDirection: "asc",
-        },
-        {
-          key: "updateDate",
-          title: "Last Update Date",
-          sortable: true,
-          sortDirection: "asc",
-        },
-      ],
-      groupOptions: [
-        { text: "Schools", value: "schools" },
-        { text: "Students", value: "students" },
-      ],
+      tab: null,
     };
-  },
-  computed: {
-    ...mapState(useAppStore, {
-      getSchoolsList: "getSchoolsList",
-    }),
-  },
-  methods: {
-    keyHandler: function (e) {
-      if (e.keyCode === 13) {
-        //enter key pressed
-        this.schoolReportSearch();
-      }
-    },
-    schoolTitle(item) {
-      if (item) {
-        return `${item.mincode} - ${item.displayName}`;
-      } else {
-        return null;
-      }
-    },
-    // schoolReportSearch() {
-    //   //reset results
-    //   this.totalResults = "";
-    //   this.searchMessage = "";
-    //   this.searchLoading = true;
-
-    //   if (!this.mincode.value) {
-    //     this.totalResults = "";
-    //     this.searchLoading = false;
-    //     this.searchMessage = "Enter a school mincode to view reports.";
-    //   } else {
-    //     GraduationReportService.getAllReportsForSchool(
-    //       this.mincode.value + (this.mincode.contains ? "*" : "")
-    //     )
-    //       .then((response) => {
-    //         this.reports = response.data;
-    //         this.searchLoading = false;
-    //         this.totalResults = response.data.length;
-    //         if (response.data.length < 1) {
-    //           this.searchMessage = "No reports found for this school";
-    //         }
-    //       })
-    //       .catch((error) => {
-    //         this.searchLoading = false;
-    //         this.searchMessage = "No reports found for this school";
-    //         this.snackbarStore.showSnackbar(error.message, "error", 5000);
-    //       });
-    //   }
-    // },
-    schoolReportSearch() {
-      //reset results
-      this.totalResults = "";
-      this.searchMessage = "";
-      this.searchLoading = true;
-
-      console.log(this.searchSchoolId)
-      if (!this.searchSchoolId) {
-        this.totalResults = "";
-        this.searchLoading = false;
-        this.searchMessage = "Select a school to view reports.";
-      } else {
-        GraduationReportService.getAllReportsForSchool(this.searchSchoolId)
-          .then((response) => {
-            this.reports = response.data;
-            this.searchLoading = false;
-            this.totalResults = response.data.length;
-            if (response.data.length < 1) {
-              this.searchMessage = "No reports found for this school";
-            }
-          })
-          .catch((error) => {
-            this.searchLoading = false;
-            this.searchMessage = "No reports found for this school";
-            this.snackbarStore.showSnackbar(error.message, "error", 5000);
-          });
-      }
-    },
-    clearInput: function () {
-      this.reports = "";
-      this.mincode.value = "";
-      this.mincode.contains = false;
-    },
-    downloadFile: function (data, mimeType, filename) {
-      sharedMethods.base64ToFileTypeAndDownload(data, mimeType, filename);
-    },
   },
 };
 </script>
 
 <style scoped>
-.table th,
-.table td {
-  border-top: none !important;
-}
-.advanced-search-form {
-  background-color: #fff;
-}
-.wild-card-button:hover {
-  cursor: pointer;
-}
-.wild-card-button {
-  color: #dee2eb;
-  position: absolute;
-  right: 21px;
-  top: 10px;
-  z-index: 10;
-  text-decoration: none;
-}
-
-.wild-card-button:visited {
-  color: #dee2eb;
-  text-decoration: none;
-}
-
-.wild-card-button.active {
-  color: green;
-}
-.search-results-message {
-  float: left;
-  clear: both;
-}
-.advanced-search-button {
-  padding-left: 1 5px;
-}
 .tab-loading {
   color: green !important;
 }
@@ -304,11 +52,23 @@ export default {
 .profile-name {
   padding-bottom: 10px;
 }
-.table-filter {
-  top: 0px !important;
-}
 .reports-view {
   padding-left: 25px;
   padding-right: 25px;
+}
+
+.table th,
+.table td {
+  border-top: none !important;
+}
+.advanced-search-form {
+  background-color: #fff;
+}
+.search-results-message {
+  float: left;
+  clear: both;
+}
+.advanced-search-button {
+  padding-left: 1 5px;
 }
 </style>
