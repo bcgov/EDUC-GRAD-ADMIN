b722e084b3f5861e64da0707ccfcbfb31b4e6d09
Shaun Lum <shaun.lum@gov.bc.ca>
2025-01-24 12:05:24 -0800

    updaed trax router to accept any version

--- tools/openshift/update-configmap-backend.sh
+++ tools/openshift/update-configmap-backend.sh
@@ -95,8 +95,7 @@ oc create -n "$OPENSHIFT_NAMESPACE" configmap "$APP_NAME"-backend-config-map \
   --from-literal=ASSESSMENT_API_URL="http://educ-grad-assessment-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080/api/v1" \
   --from-literal=GRADUATION_REPORT_API_URL="http://educ-grad-graduation-report-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080/api/v1" \
   --from-literal=GRAD_REPORT_API_URL="http://educ-grad-report-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=GRAD_TRAX_API_URL="http://educ-grad-trax-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080/api/v1" \
-  --from-literal=GRAD_TRAX_API_URL_V2="http://educ-grad-trax-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080/api/v2" \
+  --from-literal=GRAD_TRAX_API_URL="http://educ-grad-trax-api.$GRAD_NAMESPACE-$ENV.svc.cluster.local:8080" \
   --from-literal=INSTITUTE_API_URL="http://institute-api-master.$COMMON_NAMESPACE-$ENV.svc.cluster.local:8080/api/v1" \
   --dry-run=client -o yaml | oc apply -f -
 
