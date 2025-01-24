42878ff4a2d762976f58254441947c98d8aa5a3f
Shaun Lum <shaun.lum@gov.bc.ca>
2025-01-24 14:56:10 -0800

    updated assessment router

--- backend/src/routes/assessments-router.js
+++ backend/src/routes/assessments-router.js
@@ -29,10 +29,12 @@ async function getAssessmentAPI(req, res) {
   const token = getBackendToken(req);
   const version = req.version;
   try {
-    const url = `${config.get('server:assessmentAPIURL')}/api/${version}/assessment${req.url}` ;
+   let url = `${config.get('server:assessmentAPIURL')}/api/${version}/assessment` ;
+
     if (req.url != "/") {
       url += req.url;
     }
+    console.log(url)
     const data = await getData(token, url, req.session?.correlationID);
     return res.status(200).json(data);
   } catch (e) {
