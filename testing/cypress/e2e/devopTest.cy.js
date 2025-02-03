f7a49b46269af3526b16131eb7298d5c2794659d
KentoHyono <keeeeento4649@icloud.com>
2025-02-03 12:13:05 -0800

    Testing if workflow is loading env file

--- testing/cypress/e2e/devopTest.cy.js
+++ testing/cypress/e2e/devopTest.cy.js
@@ -5,9 +5,9 @@ describe('Test for cypress deployment', () => {
       'To access the Graduation Administration Application, you must have a valid IDIR.'
     )
 
+    cy.log(Cypress.env('username'))
     cy.login()
     cy.visit('/')
-
     cy.get('body').should('contain.text', 
       'PEN Search'
     )
