84e66f3e14a5285f5f0833674b5608354b96a9e6
KentoHyono <keeeeento4649@icloud.com>
2025-02-05 14:31:25 -0800

    Fixed batchTable.cy.js for when batch job status is not compelted

--- testing/cypress/e2e/batchProcessing/batchTables.cy.js
+++ testing/cypress/e2e/batchProcessing/batchTables.cy.js
@@ -4,27 +4,33 @@ const batchProcessingSelectors = selectors.batchProcessing
 // Test case changes if selected run have 0 update student or not
 function checkResultTable(rowIndex) {
   const currentBatchTable = batchProcessingSelectors.activeWindow + " " + batchProcessingSelectors.batchTable
-  // Get 'Actual' affected students
-  cy.get(currentBatchTable).find(batchProcessingSelectors.rows)
-    .eq(rowIndex).children().eq(8).invoke('text').then(text => {
-      let noData = text == '0'
-      cy.get(currentBatchTable).find(batchProcessingSelectors.rows)
-      .eq(rowIndex).children().eq(1).find('button').click({force: true})
-      cy.get(batchProcessingSelectors.viewBatchResultBtn).click({force: true})
-      cy.wait(1000)
+  // Check if there is data to display in batch result table by seeting status and actual result
+  const columns = () => cy.get(currentBatchTable).find(batchProcessingSelectors.rows).eq(rowIndex).children()
+  // Status
+  columns().eq(6).invoke('text').then(status => {
+      let hasCompleted = status.trim() == 'COMPLETED'
+      // Actual result
+      columns().eq(8).invoke('text').then(actualResult => {
+        let noData = actualResult.trim() == '0'
 
-      // Assert
-      const batchJobResultTable = batchProcessingSelectors.activeWindow + " " + batchProcessingSelectors.batchJobResultTable
-      if (noData) {
-        cy.get(batchJobResultTable).find(batchProcessingSelectors.batchJobResultNoData).should('have.text', 'No data available')
-      } else {
-        cy.get(batchJobResultTable).find(batchProcessingSelectors.rows).its('length').should('be.gt', 0) // TODO: It is only reading 10 rows wher it should read all rows
-        cy.get(batchJobResultTable).find(batchProcessingSelectors.firstRow).children().eq(0).find('button').click({force: true})
-        cy.url().should('include', Cypress.config('baseUrl') + "/student-profile")
-        cy.get(batchProcessingSelectors.navBtn).click()
-      }
-
-      cy.wait(1000)
+        cy.get(currentBatchTable).find(batchProcessingSelectors.rows)
+        .eq(rowIndex).children().eq(1).find('button').click({force: true})
+        cy.get(batchProcessingSelectors.viewBatchResultBtn).click({force: true})
+        cy.wait(1000)
+  
+        // Assert
+        const batchJobResultTable = batchProcessingSelectors.activeWindow + " " + batchProcessingSelectors.batchJobResultTable
+        if (noData || !hasCompleted) {
+          cy.get(batchJobResultTable).find(batchProcessingSelectors.batchJobResultNoData).should('have.text', 'No data available')
+        } else {
+          cy.get(batchJobResultTable).find(batchProcessingSelectors.rows).its('length').should('be.gt', 0) // TODO: It is only reading 10 rows wher it should read all rows
+          cy.get(batchJobResultTable).find(batchProcessingSelectors.firstRow).children().eq(0).find('button').click({force: true})
+          cy.url().should('include', Cypress.config('baseUrl') + "/student-profile")
+          cy.get(batchProcessingSelectors.navBtn).click()
+        }
+  
+        cy.wait(1000)
+      })
     })
 }
 
