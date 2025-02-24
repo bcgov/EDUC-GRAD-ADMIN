9ae31d0025b5001558a7d0c0cb7c46c9a61ebe74
suzalflueck <samflueck95@gmail.com>
2025-02-24 15:36:43 -0800

    Added school status badge to School Select components on UI. OpenStatusBadge is shared component for reuse accross UI

--- frontend/src/components/Common/OpenStatusBadge.vue
+++ frontend/src/components/Common/OpenStatusBadge.vue
@@ -1,12 +1,52 @@
 <template>
-  <v-chip>Hello World</v-chip>
+  <v-chip
+    v-if="getSchoolOpenStatus == 'Open'"
+    color="success"
+    variant="flat"
+    :density="compact ? 'compact' : 'default'"
+    >Open</v-chip
+  >
+  <v-chip
+    v-else-if="getSchoolOpenStatus == 'Closed'"
+    color="error"
+    variant="flat"
+    :density="compact ? 'compact' : 'default'"
+    >Closed</v-chip
+  >
+  <v-chip
+    v-else-if="getSchoolOpenStatus == 'Opening'"
+    color="primary"
+    variant="flat"
+    :density="compact ? 'compact' : 'default'"
+    >Opening</v-chip
+  >
+  <v-chip
+    v-else-if="getSchoolOpenStatus == 'Closing'"
+    color="warn"
+    variant="flat"
+    :density="compact ? 'compact' : 'default'"
+    >Closing</v-chip
+  >
 </template>
 
 <script>
+import sharedMethods from "@/sharedMethods.js";
+
 export default {
   props: {
-    openedDate: string,
-    closedDate: string,
+    // school: Object,
+    openedDateString: String,
+    closedDateString: String,
+    compact: { type: Boolean, default: true },
+  },
+  computed: {
+    getSchoolOpenStatus() {
+      //return sharedMethods.getSchoolOpenStatus(this.school);
+      return sharedMethods.getSchoolOpenStatus(
+        this.openedDateString,
+        this.closedDateString
+      );
+    },
   },
 };
 </script>
