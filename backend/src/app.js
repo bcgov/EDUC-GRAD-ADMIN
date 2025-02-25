f0b2342ea83f5a3492e6960b156d2196282243f8
Chris Ditcher <chris.ditcher@gov.bc.ca>
2025-02-25 14:46:05 -0800

    Test (#769)

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const passport = require("passport");
 const helmet = require("helmet");
 const cors = require("cors");
 //const utils = require("./components/utils");
-// test comment
+// test comment change
 const auth = require("./components/auth");
 const bodyParser = require("body-parser");
 const connectRedis = require("connect-redis");
