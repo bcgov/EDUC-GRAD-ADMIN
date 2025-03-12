fb7e6a4534a7166a713f73ee5e58a2c932f98895
Hyono <kento.hyono@gov.bc.ca>
2025-03-12 16:52:39 -0700

    Removed previous testing folder

--- testing/customDoc/custom-headings.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const headings = document.querySelectorAll("h3.subsection-title");
-  headings.forEach((heading) => {
-      // Replace "Member" with "Tests" in the text content of each heading
-      if (heading.textContent.includes("Members"))
-        heading.textContent = heading.textContent.replace(/Members/g, "Tests");
-  });
-});
