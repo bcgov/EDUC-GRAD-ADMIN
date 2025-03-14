324ede1bf439814415eabcf41de6a099576a9dbd
Hyono <kento.hyono@gov.bc.ca>
2025-03-14 15:04:59 -0700

    added testing for reprint certificate

---
+++ tests-e2e/cypress/e2e/batchProcessing/reprintCertificate.cy.js
@@ -0,0 +1,8 @@
+import selectors from "../../support/selectors";
+const batchProcessingSelectors = selectors.batchProcessing
+
+describe('Reprint Certificate without principal signature', () => {
+  const batch_test_student = Cypress.env('test_student').graduated_student
+
+
+})
