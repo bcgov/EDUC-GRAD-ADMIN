83b29bbc545ee3ccac6de386fd21bf3f17aa2fd7
Hyono <kento.hyono@gov.bc.ca>
2025-03-21 10:59:41 -0700

    Added documentation for certificateRegeneration

--- tests-e2e/cypress/e2e/batchRequests/certificateRegeneration.cy.ts
+++ tests-e2e/cypress/e2e/batchRequests/certificateRegeneration.cy.ts
@@ -1,3 +1,11 @@
+/**
+ * @module certificateRegeneration
+ * 
+ * @description
+ * Run "User Request Certificate Regeneration" in Batch Processing.  This spec tests CERTREGEN by calling API directly
+ * instead of naviagting thruogh UI to speed up the testing, as well as making sure endpoints work.
+ */
+
 import selectors from "../../support/selectors";
 import { formatTime, getCurrentTimestamp, isWithinMarginSeconds, base64ToFileTypeAndDownload } from "../../support/helperMethods"
 const { deleteDownloadsFolderBeforeAll } = require('cypress-delete-downloads-folder');
@@ -18,6 +26,30 @@ describe('User Request Certificate Regeneration', () => {
     cy.wait(500)
   })
 
+  /**
+   * @name runsOnStudent
+   * 
+   * @description
+   * Run Certificate Regeneration for a single student to make sure student's data is properly updated.
+   * 
+   * ## Steps:
+   * 1. Open User Request Certificate Regeneration modal on Batch Processing
+   * 2. Select Student as a group
+   * 3. Enter PEN
+   * 4. Click Add Student and Next
+   * 5. Select Download option and Next
+   * 6. Wait a little bit for letting app store load
+   * 7. Set up network interception for executecertregenbatchjob to get a batch id from response
+   * 8. Click Submit
+   *    - This will call executecertregenbatchjob and Cypress obtains a batch id out of the response
+   * 9. Call batch summary endpoint repeatedly until it's completed
+   *    - If it's not completed within set timeout, test fails
+   * 10. Call batch history for the batch id to ensure data is valid
+   *    - activityCode should be CERTREGEN
+   *    - Length of content should be 1
+   *    - updateDate of a student should be same/close to current date time
+   * 11. Call graduation report API for checking updateDate for certificate is updated
+   */
   it('Runs Certificate Regeneration on a student', () => {
     cy.get(batchProcessingSelectors.overlayWindow).find(batchProcessingSelectors.selectInput).click({force: true})
     cy.get(selectors.selections).contains('Student').click()
