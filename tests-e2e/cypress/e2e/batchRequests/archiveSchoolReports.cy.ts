6928ea99d9907f7f026cd1c06a1076218f82fbc5
Hyono <kento.hyono@gov.bc.ca>
2025-03-26 16:34:42 -0700

    Done documentation for archive school report

--- tests-e2e/cypress/e2e/batchRequests/archiveSchoolReports.cy.ts
+++ tests-e2e/cypress/e2e/batchRequests/archiveSchoolReports.cy.ts
@@ -1,3 +1,11 @@
+/**
+ * @module ArchiveSchoolReport
+ * 
+ * @description
+ * Run "Archive School Reports Process" in Batch Processing. This spec tests Archive School Reports by calling API directly
+ * instead of naviagting thruogh UI to speed up the testing, as well as making sure endpoints work. 
+ */
+
 import { formatTime, getCurrentTimestamp, isWithinMarginSeconds } from "tests-e2e/cypress/support/helperMethods";
 import selectors from "../../support/selectors";
 const batchProcessingSelectors = selectors.batchProcessing
@@ -33,6 +41,26 @@ describe('Archive School Reports', () => {
     cy.get(batchProcessingSelectors.overlayWindow).contains('Submit').click()
   })
 
+  /**
+   * @name runsOnProjectedNonGraduates
+   * 
+   * @description
+   * Run Archive School Reports Process on Projected Non-graduates student to make sure data is
+   * properly updated.
+   * 
+   * ## Steps:
+   * 1. Open Archive School Reports modal on Batch Processing
+   * 2. Select Projected Non-graduates as a report type
+   * 3. Add a school and Next
+   * 4. Set up network interception for archive to get a batch id from response
+   * 5. Click Submit 
+   *    - This will call archive run and Cypress obtains a batch id out of the response
+   * 6. Call batch summary endpoint repeatedly until it's completed
+   *    - If it's not completed within set timeout, test fails
+   * 7. Call graduation report API by school GUID to get school reports and make sure data is valid
+   *    - NONGRADPRJ type should no longer exist
+   *    - NONGRADPRJARC type should exist and updateDate is updated
+   */
   it.only('Archives NONGRADPRJ', () => { 
     cy.get(batchProcessingSelectors.overlayWindow).find(batchProcessingSelectors.selectInput).first().click({force: true})
     cy.get(selectors.selections).contains('NONGRADPRJ').click({force: true})
