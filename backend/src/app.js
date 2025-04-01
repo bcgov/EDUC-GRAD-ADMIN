22af736c26fbc1e448b3d8e6a886a97dd26d668b
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2025-04-01 09:02:40 -0700

    silent idir login (#803)
    * Fixed symantec error
    
    * Update grad-version
    
    * Update grad-version
    
    Updated test to 1.28.0
    
    * silent idir login
    
    * silent idir login
    
    ---------
    
    Co-authored-by: cditcher <chris.ditcher@gov.bc.ca>
    Co-authored-by: githubmamatha <106563495+githubmamatha@users.noreply.github.com>

--- backend/src/app.js
+++ backend/src/app.js
@@ -153,6 +153,28 @@ auth
         }
       )
     );
+    passport.use('oidcIDIRSilent', new OidcStrategy({
+      issuer: discovery.issuer,
+      authorizationURL: discovery.authorization_endpoint,
+      tokenURL: discovery.token_endpoint,
+      userInfoURL: discovery['userinfo_endpoint'],
+      clientID: config.get('oidc:clientId'),
+      clientSecret: config.get('oidc:clientSecret'),
+      callbackURL: config.get('server:frontend') + '/api/auth/callback_idir_silent',
+      scope: 'openid profile',
+      kc_idp_hint: config.get('server:idirIDPHint')
+    }, (_issuer, _sub, profile, accessToken, refreshToken, done) => {
+      if ((typeof (accessToken) === 'undefined') || (accessToken === null) ||
+          (typeof (refreshToken) === 'undefined') || (refreshToken === null)) {
+        return done('No access token', null);
+      }
+      //Generate token for frontend validation
+      //set access and refresh tokens
+      profile.jwtFrontend = auth.generateUiToken();
+      profile.jwt = accessToken;
+      profile.refreshToken = refreshToken;
+      return done(null, profile);
+    }));
     //JWT strategy is used for authorization
     passport.use(
       "jwt",
