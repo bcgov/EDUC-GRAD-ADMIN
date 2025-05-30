2d62355663c68675f1316920feb74d9ef7709bb8
Shaun Lum <shaun.lum@gov.bc.ca>
2025-05-30 13:13:49 -0700

    added styling and format as design

--- frontend/src/components/StudentProfile/Forms/StudentCoursesCreateForm.vue
+++ frontend/src/components/StudentProfile/Forms/StudentCoursesCreateForm.vue
@@ -12,7 +12,6 @@
     </template>
 
     <v-card>
-      {{ coursesToCreate }}
       <v-card-title>
         <v-row no-gutters>
           <div class="v-card-title">Add Student Courses</div>
@@ -32,9 +31,16 @@
       <v-stepper alt-labels show-actions v-model="step">
         <template v-slot:default>
           <v-stepper-header>
-            <v-stepper-item title="Select Courses" value="0" />
-            <v-stepper-item title="Enter Details" value="1" />
-            <v-stepper-item title="Confirmation" value="2" />
+            <v-stepper-item title="Select Courses" value="0">
+              <template #icon> 1 </template></v-stepper-item
+            >
+            <v-stepper-item title="Enter Details" value="1">
+              <template #icon> 2 </template></v-stepper-item
+            >
+
+            <v-stepper-item title="Confirmation" value="2">
+              <template #icon> 3 </template></v-stepper-item
+            >
           </v-stepper-header>
 
           <v-stepper-window>
@@ -47,18 +53,21 @@
                   label="Course Code"
                   variant="outlined"
                   density="compact"
+                  class="pr-1"
                 />
                 <v-text-field
                   v-model="courseAdd.level"
                   label="Course Level"
                   variant="outlined"
                   density="compact"
+                  class="pr-1"
                 />
                 <v-text-field
                   v-model="courseAdd.courseSession"
-                  label="Course Session"
+                  label="Session Date"
                   variant="outlined"
                   density="compact"
+                  class="pr-1"
                 />
                 <v-btn
                   variant="flat"
@@ -70,6 +79,7 @@
               </v-row>
 
               <v-data-table
+                v-if="coursesToCreate.length"
                 :items="coursesToCreate"
                 :headers="headers"
                 item-value="id"
@@ -106,17 +116,124 @@
 
             <!-- Step 3 -->
             <v-stepper-window-item value="2">
-              <pre>{{
-                JSON.stringify(createStudentResultsMessages, null, 2)
-              }}</pre>
-              <v-data-table
-                v-if="coursesToCreate"
-                :headers="tableHeaders"
-                :items="coursesToCreate"
-                item-value="courseID"
-                class="elevation-1"
+              <v-expansion-panels multiple>
+                <v-expansion-panel
+                  v-for="(course, index) in createStudentResultsMessages"
+                  :key="index"
+                >
+                  <v-expansion-panel-title
+                    class="d-flex align-center justify-space-between"
+                  >
+                    <div class="d-flex align-center flex-grow-1">
+                      <v-icon
+                        :color="
+                          course.validationIssues.some(
+                            (i) => i.validationIssueSeverityCode === 'ERROR'
+                          )
+                            ? 'error'
+                            : 'success'
+                        "
+                        class="mr-2"
+                      >
+                        {{
+                          course.validationIssues.some(
+                            (i) => i.validationIssueSeverityCode === "ERROR"
+                          )
+                            ? "mdi-close-circle"
+                            : "mdi-check-circle"
+                        }}
+                      </v-icon>
+
+                      <span>
+                        {{ course.originalCourse.courseCode }}
+                        {{ course.originalCourse.courseLevel }} –
+                        {{ course.originalCourse.courseSession }}
+                        {{
+                          course.validationIssues.some(
+                            (i) => i.validationIssueSeverityCode === "ERROR"
+                          )
+                            ? " failed to add course"
+                            : ` added to student ${studentPen}`
+                        }}
+                      </span>
+
+                      <v-spacer />
+
+                      <span class="text-caption text-grey-darken-1">
+                        {{
+                          (() => {
+                            const errors = course.validationIssues.filter(
+                              (i) => i.validationIssueSeverityCode === "ERROR"
+                            ).length;
+                            const warnings = course.validationIssues.filter(
+                              (i) => i.validationIssueSeverityCode === "WARNING"
+                            ).length;
+
+                            const parts = [];
+                            if (errors > 0) parts.push(`${errors} error(s)`);
+                            if (warnings > 0)
+                              parts.push(`${warnings} warning(s)`);
+                            return parts.join(", ");
+                          })()
+                        }}
+                      </span>
+                    </div>
+
+                    <template #actions>
+                      <v-icon>mdi-chevron-down</v-icon>
+                    </template>
+                  </v-expansion-panel-title>
+
+                  <v-expansion-panel-text>
+                    <div v-if="course.validationIssues.length">
+                      <v-alert
+                        v-for="(issue, i) in course.validationIssues"
+                        :key="i"
+                        :type="
+                          issue.validationIssueSeverityCode === 'ERROR'
+                            ? 'error'
+                            : 'warning'
+                        "
+                        dense
+                        outlined
+                        class="mb-2"
+                      >
+                        {{ issue.validationIssueMessage }}
+                      </v-alert>
+                    </div>
+                    <div v-else>
+                      <v-alert type="success" dense outlined>
+                        No validation issues.
+                      </v-alert>
+                    </div>
+                    <pre>{{ course.originalCourse }}</pre>
+                  </v-expansion-panel-text>
+                </v-expansion-panel>
+              </v-expansion-panels>
+
+              <v-alert
+                v-if="coursesToCreate.length > 0"
+                type="info"
+                class="mb-4"
+                border="start"
+                elevation="2"
+                variant="tonal"
               >
-              </v-data-table>
+                <div class="mb-2">
+                  You are about to add the following courses to student
+                  <strong>{{ studentPen }}</strong
+                  >:
+                </div>
+                <ul class="pl-4">
+                  <li v-for="course in coursesToCreate" :key="course.courseID">
+                    <strong
+                      >{{ course.courseCode }} {{ course.courseLevel }} –
+                      {{ course.courseSession }}
+                    </strong>
+                    {{ course.courseName }}
+                  </li>
+                </ul>
+              </v-alert>
             </v-stepper-window-item>
           </v-stepper-window>
         </template>
@@ -139,7 +256,7 @@
           @click="step--"
           color="bcGovBlue"
           variant="outlined"
-          :disabled="disableBackButton"
+          :disabled="step == 0 || validationStep"
         >
           Back
         </v-btn>
@@ -156,13 +273,22 @@
         </v-btn>
 
         <v-btn
-          v-else
+          v-else-if="step == 2 && !validationStep"
           @click="submitForm"
           color="error"
           variant="flat"
           class="text-none"
         >
           Add Student Courses
+        </v-btn>
+        <v-btn
+          v-else-if="step == 2 && validationStep"
+          @click="closeCreateStudentCourseDialog"
+          color="error"
+          variant="flat"
+          class="text-none"
+        >
+          Close
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -187,6 +313,7 @@ export default {
       createStudentResultsMessages: [],
       dialog: false,
       step: 0,
+      validationStep: false,
 
       courseAdd: {
         code: null,
@@ -194,10 +321,13 @@ export default {
         courseSession: null,
       },
       headers: [
-        { title: "Course Name", key: "courseName" },
-        { title: "Course Level", key: "courseLevel" },
-        { title: "Course Session", key: "courseSession" },
-        { title: "Remove", key: "remove" },
+        { title: "Code", key: "courseCode" },
+        { title: "level", key: "courseLevel" },
+        { title: "Session Date", key: "courseSession" },
+        { title: "Title", key: "courseName" },
+        { title: "Start Date", key: "startDate" },
+        { title: "End Date", key: "endDate" },
+        { title: "", key: "remove" },
       ],
     };
   },
@@ -205,6 +335,7 @@ export default {
     ...mapState(useAccessStore, ["hasPermissions"]),
     ...mapState(useStudentStore, {
       coursesToCreate: (state) => state.create.courses,
+      studentPen: "getStudentPen",
     }),
   },
   methods: {
@@ -218,6 +349,8 @@ export default {
     openCreateStudentCoursesDialog() {
       this.step = 0;
       this.dialog = true;
+      this.createStudentResultsMessages = [];
+      this.clearForm();
     },
 
     closeCreateStudentCourseDialog() {
@@ -240,6 +373,8 @@ export default {
         const courseName = response?.data?.courseName;
         const courseCode = response?.data?.courseCode;
         const courseLevel = response?.data?.courseLevel;
+        const startDate = response?.data?.startDate;
+        const endDate = response?.data?.endDate;
         const genericCourseType = response?.data?.genericCourseType;
 
         if (courseID) {
@@ -249,6 +384,8 @@ export default {
             courseLevel,
             courseSession,
             courseName,
+            startDate,
+            endDate,
             genericCourseType,
           });
           this.clearForm();
@@ -261,10 +398,18 @@ export default {
     },
 
     clearForm() {
-      this.courseAdd = { code: null, level: null, courseSession: null };
+      this.courseAdd = {
+        code: null,
+        level: null,
+        courseSession: null,
+        courseStartDate: null,
+        courseEndDate: null,
+      };
+      this.validationStep = false;
     },
 
     async submitForm() {
+      this.createStudentResultsMessages = [];
       const createStudentCoursesRequestBody = toRaw(this.coursesToCreate);
 
       // Call API and get response
@@ -272,13 +417,20 @@ export default {
         createStudentCoursesRequestBody
       );
 
-      this.createStudentResultsMessages = response; // still store full response if needed
-      console.log(this.createStudentResultsMessages);
-      this.clearCoursesToCreate(); // optional depending on if you still need to show messages
-    },
+      // Enrich response with entire original course object
+      const enrichedResults = response.map((result) => {
+        const original = this.coursesToCreate.find(
+          (course) => course.courseID === result.courseID
+        );
+        return {
+          ...result,
+          originalCourse: original || {},
+        };
+      });
 
-    disableBackButton() {
-      return false;
+      this.createStudentResultsMessages = enrichedResults;
+      this.clearCoursesToCreate(); // optional
+      this.validationStep = true;
     },
   },
 };
