c94ebfe54e10a209abcff5751dffe7ea422bb728
Tang <michael.tang@gov.bc.ca>
2025-06-10 10:27:45 -0700

    added changes for Nancy

--- frontend/src/components/Codes/StudentGradeCodes.vue
+++ frontend/src/components/Codes/StudentGradeCodes.vue
@@ -11,10 +11,10 @@
       showFilter="true"
     >
       <template v-slot:item.effectiveDate="{ item }">
-        {{ $filters.dateFormatYYYYMMDD(item.effectiveDate) }}
+        {{ $filters.formatSimpleDate(item.effectiveDate) }}
       </template>
       <template v-slot:item.expiryDate="{ item }">
-        {{ $filters.dateFormatYYYYMMDD(item.expiryDate) }}
+        {{ $filters.formatSimpleDate(item.expiryDate) }}
       </template>
     </DisplayTable>
   </div>
@@ -25,6 +25,7 @@ import DisplayTable from "@/components/DisplayTable.vue";
 import { useSnackbarStore } from "@/store/modules/snackbar";
 import { mapState, mapActions } from "pinia";
 import { useAppStore } from "@/store/modules/app";
+
 export default {
   name: "GradeCodes",
   components: {
