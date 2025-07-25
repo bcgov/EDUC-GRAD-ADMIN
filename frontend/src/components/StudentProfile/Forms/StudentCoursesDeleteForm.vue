de31a8e394ff2e8f335e54bb114617aab26e4bfb
Shaun Lum <shaun.lum@gov.bc.ca>
2025-07-25 15:39:30 -0700

    delete updated

--- frontend/src/components/StudentProfile/Forms/StudentCoursesDeleteForm.vue
+++ frontend/src/components/StudentProfile/Forms/StudentCoursesDeleteForm.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <v-dialog v-model="dialog" persistent max-width="1260">
+    <v-dialog v-model="dialog" persistent max-width="760">
       <template v-slot:activator="{ props }">
         <slot name="activator" v-bind="props">
+
           <v-btn v-if="
             hasPermissions('STUDENT', 'courseUpdate') && courseBatchDelete
           " v-bind="props" :disabled="selectedCoursesToDelete.length === 0" color="error" class="text-none"
@@ -10,8 +11,12 @@
             Delete Selected Courses
           </v-btn>
 
-          <v-btn v-else-if="!courseBatchDelete" :disabled="!hasPermissions('STUDENT', 'courseUpdate')" v-bind="props"
+          <v-btn v-else-if="!courseBatchDelete"
+            :disabled="!hasPermissions('STUDENT', 'courseUpdate') || studentStatus == 'MER'" v-bind="props"
             color="error" icon="mdi-delete-forever" density="compact" variant="text" />
+          {{ selectedCoursesWithValidations.error.length }}
+          {{ selectedCoursesWithValidations.warning.length }}
+          {{ selectedCoursesWithValidations.info.length }}
         </slot>
       </template>
 
@@ -26,67 +31,109 @@
           <v-card-subtitle>{{
             studentStore.formattedStudentName
           }}</v-card-subtitle>
+
         </v-card-title>
-
         <v-card-text>
-          <div class="mb-2" v-if="selectedCoursesWithWarnings.length > 0">
-            You are about to remove the following student course{{
-              selectedCoursesWithWarnings.length > 1 ? "s" : ""
-            }}:
+          <StudentCourseAlert :studentStatus="studentStatus" />
+          <!-- Warnings & Info: These will be deleted -->
+          <div v-if="selectedCoursesWithValidations.warning.length || selectedCoursesWithValidations.info.length">
+            <h4 class="font-weight-bold my-2">The following courses will be deleted</h4>
+
+            <div
+              v-for="(course, index) in [...selectedCoursesWithValidations.warning, ...selectedCoursesWithValidations.info]"
+              :key="'deletable-' + index" class="mb-3">
+              <v-alert :type="course.validationType" border="start" variant="tonal" density="compact"
+                icon="mdi-alert-circle">
+                <v-row class="mb-2">
+                  <v-col cols="12" v-if="course.validationMessage">
+                    <div class="d-flex align-center mb-1">
+                      <span>{{ course.validationMessage }}</span>
+                    </div>
+                  </v-col>
+                  <v-col cols="12">
+                    <strong>{{ course.courseDetails.courseCode }} {{ course.courseDetails.courseLevel }} -
+                      {{ $filters.formatYYYYMMStringDate(course.courseSession) }}</strong>
+                    <v-spacer /> ({{ course.courseDetails.courseName }})
+                  </v-col>
+                  <v-col class="ml-3"><strong>Interim</strong>&nbsp;
+                    <span v-if="course.interimPercent">{{ course.interimPercent }}% {{ course.interimLetterGrade
+                    }}</span>
+                    <span v-else><i>null</i></span>
+                  </v-col>
+                  <v-col><strong>Final</strong>&nbsp;
+                    <span v-if="course.finalPercent">{{ course.finalPercent }}% {{ course.finalLetterGrade }}</span>
+                    <span v-else><i>null</i></span>
+                  </v-col>
+                  <v-col><strong>Credits</strong> {{ course.credits }}</v-col>
+                  <v-col><strong>FA/AS</strong>&nbsp;
+                    <span v-if="course.fineArtsAppliedSkills">{{ course.fineArtsAppliedSkills }}</span>
+                    <span v-else><i>null</i></span>
+                  </v-col>
+                  <v-col><strong>Eq/Ch</strong>&nbsp;
+                    <span v-if="course.equivOrChallenge">{{ course.equivOrChallenge }}</span>
+                    <span v-else><i>null</i></span>
+                  </v-col>
+                  <v-col cols="12" class="ml-3" v-if="course.customizedCourseName">
+                    <strong>Custom Course Title</strong> {{ course.customizedCourseName }}
+                  </v-col>
+
+                </v-row>
+              </v-alert>
+            </div>
           </div>
-          <v-alert type="info" variant="outline" class="mb-4" border="start" elevation="2" no-gutters
-            v-for="course in selectedCoursesWithWarnings" :key="course.id">
-            <v-row class="mb-2">
 
-              <v-col cols="12">
-                <strong>{{ course.courseDetails.courseCode }}
-                  {{ course.courseDetails.courseLevel }} -
-                  {{
-                    $filters.formatYYYYMMStringDate(course.courseSession)
-                  }}</strong> <v-spacer />({{ course.courseDetails.courseName }}) </v-col>
-              <v-col class="ml-3"><strong>Interim</strong>&nbsp;
-                <span v-if="course.interimPercent">{{ course.interimPercent }}%
-                  {{ course.interimLetterGrade }}</span>
-                <span v-else> <i>null</i> </span>
-              </v-col>
-              <v-col><strong>Final</strong>&nbsp;
-                <span v-if="course.finalPercent">
-                  {{ course.finalPercent }}% {{ course.finalLetterGrade }}</span><span v-else><i>null</i></span></v-col>
-              <!-- I don't think credits can have a null value? - Samara -->
-              <v-col><strong>Credits</strong> {{ course.credits }}</v-col>
-              <v-col><strong>FA/AS</strong>&nbsp;
-                <span v-if="course.fineArtsAppliedSkills">
-                  {{ course.fineArtsAppliedSkills }}
-                </span>
-                <span v-else><i>null</i></span>
-              </v-col>
-              <v-col><strong>Eq/Ch</strong>&nbsp;
-                <span v-if="course.equivOrChallenge">
-                  {{ course.equivOrChallenge }}
-                </span>
-                <span v-else><i>null</i></span>
-              </v-col>
-              <v-col cols="12" class="ml-3" v-if="course.customizedCourseName"><strong>Custom Course Title</strong>
-                {{ course.customizedCourseName }}</v-col>
-              <v-col cols="12">
-                <div v-for="(issue, index) in course.validationIssues" :key="index" class="d-flex align-center mb-1">
-                  <v-icon color="warning" class="mr-2">mdi-alert</v-icon>
-                  <span>{{ issue.message }}</span>
-                </div>
-              </v-col>
-            </v-row>
-          </v-alert>
-
+          <!-- Errors: These will NOT be deleted -->
+          <div v-if="selectedCoursesWithValidations.error.length">
+            <h4 class="font-weight-bold my-2">The following courses will NOT be deleted</h4>
+            <div v-for="(course, index) in selectedCoursesWithValidations.error" :key="'error-' + index" class="mb-3">
+              <v-alert type="error" border="start" variant="tonal" density="compact" icon="mdi-alert-circle">
+                <v-row class="mb-2">
+                  <v-col cols="12">
+                    <div class="d-flex align-center mb-1">
+                      <span>{{ course.validationMessage }}</span>
+                    </div>
+                  </v-col>
+                  <v-col cols="12">
+                    <strong>{{ course.courseDetails.courseCode }} {{ course.courseDetails.courseLevel }} -
+                      {{ $filters.formatYYYYMMStringDate(course.courseSession) }}</strong>
+                    ({{ course.courseDetails.courseName }})
+                  </v-col>
+                  <v-col class="ml-3"><strong>Interim</strong>&nbsp;
+                    <span v-if="course.interimPercent">{{ course.interimPercent }}% {{ course.interimLetterGrade
+                    }}</span>
+                    <span v-else><i>null</i></span>
+                  </v-col>
+                  <v-col><strong>Final</strong>&nbsp;
+                    <span v-if="course.finalPercent">{{ course.finalPercent }}% {{ course.finalLetterGrade }}</span>
+                    <span v-else><i>null</i></span>
+                  </v-col>
+                  <v-col><strong>Credits</strong> {{ course.credits }}</v-col>
+                  <v-col><strong>FA/AS</strong>&nbsp;
+                    <span v-if="course.fineArtsAppliedSkills">{{ course.fineArtsAppliedSkills }}</span>
+                    <span v-else><i>null</i></span>
+                  </v-col>
+                  <v-col><strong>Eq/Ch</strong>&nbsp;
+                    <span v-if="course.equivOrChallenge">{{ course.equivOrChallenge }}</span>
+                    <span v-else><i>null</i></span>
+                  </v-col>
+                  <v-col cols="12" class="ml-3" v-if="course.customizedCourseName">
+                    <strong>Custom Course Title</strong> {{ course.customizedCourseName }}
+                  </v-col>
+                </v-row>
+              </v-alert>
+            </div>
+          </div>
         </v-card-text>
-
         <v-card-actions>
           <v-btn color="error" variant="outlined" class="text-none" density="default" @click="close">
             Cancel
           </v-btn>
           <v-spacer />
-          <v-btn color="error" variant="flat" class="text-none" density="default" @click="confirmDelete">
+          <v-btn v-if="hasCoursesToDelete" color="error" variant="flat" class="text-none" density="default"
+            @click="confirmDelete">
             Delete Course<span v-if="selectedCoursesToDelete.length > 1">s</span>
           </v-btn>
+
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -97,10 +144,15 @@
 import { useSnackbarStore } from "@/store/modules/snackbar";
 import { useStudentStore } from "@/store/modules/student";
 import { useAccessStore } from "@/store/modules/access";
+import StudentCourseAlert from "@/components/StudentProfile/Forms/StudentCourseAlert.vue"
+
 
 
 export default {
   name: "StudentCoursesDeleteForm",
+  components: {
+    StudentCourseAlert,
+  },
   props: {
     selectedCoursesToDelete: {
       type: Array,
@@ -132,52 +184,74 @@ export default {
     studentPen() {
       return this.studentStore.pen;
     },
-
+    studentStatus() {
+      return this.studentStore.student.profile.studentStatus;
+    },
     requirementsMet() {
       return this.studentStore.student.gradStatus.studentGradData;
     },
+
     selectedCoursesWithWarnings() {
-      return this.selectedCoursesToDelete.map((course) => {
-        const match =
-          this.studentStore.student.gradStatus.studentGradData.studentCourses.studentCourseList.find(
-            (c) =>
-              c.courseCode === course.courseDetails.courseCode &&
-              c.courseLevel === course.courseDetails.courseLevel &&
-              c.courseSession === course.courseDetails.courseSession
+      return this.selectedCoursesWithValidations.filter(course =>
+        (!course.validationIssues || course.validationIssues.length === 0) ||
+        (
+          course.validationIssues.some(issue => issue.type === 'warning') &&
+          !course.validationIssues.some(issue => issue.type === 'error')
+        )
+      );
+    },
+
+    coursesWithNoErrorsOrWarnings() {
+      return this.selectedCoursesWithValidations.filter(course =>
+        !course.validationIssues || course.validationIssues.length === 0
+      );
+    },
+
+    hasCoursesToDelete() {
+      const { info, warning } = this.selectedCoursesWithValidations;
+      return info.length > 0 || warning.length > 0;
+    },
+
+    selectedCoursesWithValidations() {
+      const grouped = {
+        error: [],
+        warning: [],
+        info: []
+      };
+
+      this.selectedCoursesToDelete.forEach((course) => {
+        const hasExam =
+          course?.courseExam &&
+          (
+            course.courseExam.examPercentage != null ||
+            (course.courseExam.specialCase != null && course.courseExam.specialCase !== "N") ||
+            course.finalPercent != null
           );
 
-        const isUsedForGrad = match?.used == true
-        const hasExam =
-          match?.bestExamPercent !== null ||
-          (match?.specialCase && match.specialCase !== "N") ||
-          (match?.completedCoursePercentage !== null &&
-            match.completedCoursePercentage !== 0);
+        const studentIsGraduatedAndNotOnSCCP = this.studentStore.student.gradStatus?.program != "SCCP" && this.studentStore.student.gradStatus?.programCompletionDate
 
-        return {
+        let validationType = "info";
+        let validationMessage = "";
+
+        if (hasExam) {
+          validationType = "error";
+          validationMessage = "Associated Exam Record - This course has an associated exam record and cannot be deleted";
+        } else if (studentIsGraduatedAndNotOnSCCP) {
+          validationType = "warning";
+          validationMessage = "Graduated Student - ensure this course can be deleted without removing any graduation or optional program requirements.";
+        }
+
+        grouped[validationType].push({
           ...course,
-          validationIssues: [
-            ...(course.validationIssues || []),
-            ...(isUsedForGrad
-              ? [
-                {
-                  type: "warning",
-                  message:
-                    "This course is used to meet graduation requirements.",
-                },
-              ]
-              : []),
-            ...(hasExam
-              ? [
-                {
-                  type: "info",
-                  message: "This course has an associated exam record.",
-                },
-              ]
-              : []),
-          ],
-        };
+          validationType,
+          validationMessage
+        });
       });
-    },
+
+      return grouped;
+    }
+
+
   },
   methods: {
     close() {
