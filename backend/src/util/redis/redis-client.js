df9564b2ea72ab941eb07a9ad9ef53473306855c
cditcher <chris.ditcher@gov.bc.ca>
2025-08-08 10:19:48 -0700

    Removed secret

--- backend/src/util/redis/redis-client.js
+++ backend/src/util/redis/redis-client.js
@@ -21,7 +21,6 @@ const Redis = {
       redisClient = new IOREDIS.Cluster([{
         host: config.get('redis:host'),
         port: config.get('redis:port'),
-        password: config.get('redis:password')
       }]);
     }
     redisClient.on('error', (error) => {
