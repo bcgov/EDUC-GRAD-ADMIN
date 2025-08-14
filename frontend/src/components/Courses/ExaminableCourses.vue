6187ac207990ebcda60372daebc359aded7be2fa
Shaun Lum <shaun.lum@gov.bc.ca>
2025-08-14 10:36:48 -0700

    added extra info

--- frontend/src/components/Courses/ExaminableCourses.vue
+++ frontend/src/components/Courses/ExaminableCourses.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <v-alert variant="tonal" color="info" border="start" elevation="1" class="mb-4">
+      <p>
+        Courses are listed by graduation program code(s)*. For courses completed within the examinable date range, the
+        student must have written the provincial exam to earn a final mark and completed the course. When editing or
+        updating a provincially examinable course, please follow these instructions.
+      </p>
+
+      <p>
+        Use the Q-coded version if the course was completed outside B.C. or if the student was on the Adult Graduation
+        Program at the time of course completion.
+      </p>
+
+      <p>
+        *"2004+" refers to all program codes from 2004 onward.
+      </p>
+    </v-alert>
+
     <BlendingRules :expand="$route.path === '/courses/examinable-courses/blending-rules'" />
     <h3 class="ml-2 mt-5">Examinable Courses</h3>
 
