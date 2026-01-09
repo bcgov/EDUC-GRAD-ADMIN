cd550d293a96553b8cd66279f0a30dc6c5bb3046
cditcher <chris.ditcher@gov.bc.ca>
2026-01-09 14:08:50 -0800

    Added searches for status and gender

--- backend/src/components/student/studentFilters.js
+++ backend/src/components/student/studentFilters.js
@@ -6,6 +6,10 @@ function createFiltersSearchCriteria(searchFilter = []) {
 
   for (const [key, value] of Object.entries(searchFilter)) {
 
+    if (key === 'studentStatus' && value) {
+      searchCriteria.push({ key: 'studentStatus', value: value.toString(), operation: FILTER_OPERATION.EQUAL, valueType: VALUE_TYPE.STRING, condition: CONDITION.AND });
+    }
+      
     if (key === 'givenName' && value) {
       searchCriteria.push({ key: 'givenName', value: value.toString(), operation: FILTER_OPERATION.CONTAINS_IGNORE_CASE, valueType: VALUE_TYPE.STRING, condition: CONDITION.AND });
     }
@@ -16,6 +20,10 @@ function createFiltersSearchCriteria(searchFilter = []) {
 
     if (key === 'pen' && value) {
       searchCriteria.push({ key: 'pen', value: value.toString(), operation: FILTER_OPERATION.EQUAL, valueType: VALUE_TYPE.STRING, condition: CONDITION.AND });
+    }
+
+    if (key === 'genderCode' && value) {
+      searchCriteria.push({ key: 'genderCode', value: value.toString(), operation: FILTER_OPERATION.EQUAL, valueType: VALUE_TYPE.STRING, condition: CONDITION.AND });
     }
 
   }
