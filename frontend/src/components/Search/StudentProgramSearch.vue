980d344affef87e4299befaec6d5eade29c20502
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2026-01-22 10:31:43 -0800

    adult start end date

--- frontend/src/components/Search/StudentProgramSearch.vue
+++ frontend/src/components/Search/StudentProgramSearch.vue
@@ -159,9 +159,21 @@
             v-model="searchParams.adultStartDateTo"
             v-on:keyup="keyHandler"
             placeholder="YYYY-MM-DD"
+            :min="searchParams.adultStartDateFrom || undefined"
             max="9999-12-30"
             clearable
           />
+        </div>
+      </v-row>
+      <v-row v-if="adultStartDateRangeError" class="mt-0">
+        <div class="col-12">
+          <v-alert
+            type="error"
+            variant="tonal"
+            density="compact"
+            class="ml-1 py-2"
+            :text="adultStartDateRangeError"
+          ></v-alert>
         </div>
       </v-row>
       <v-row class="mt-1">
@@ -295,6 +307,17 @@ export default {
         this.searchParams.completionDateTo = null;
       }
     },
+    "searchParams.adultStartDateFrom"(newFrom) {
+      if (!newFrom) {
+        return;
+      }
+      if (
+        this.searchParams.adultStartDateTo &&
+        this.searchParams.adultStartDateTo < newFrom
+      ) {
+        this.searchParams.adultStartDateTo = null;
+      }
+    },
   },
   setup() {
     const snackbarStore = useSnackbarStore();
@@ -316,6 +339,7 @@ export default {
       searchMessage: "",
       errorMessage: "",
       dateRangeError: "",
+      adultStartDateRangeError: "",
       searchLoading: false,
       downloadLoading: false,
       totalElements: "",
@@ -506,6 +530,7 @@ export default {
       this.searchResults = [];
       this.searchMessage = "";
       this.dateRangeError = "";
+      this.adultStartDateRangeError = "";
       this.hasSearched = false;
       programSearchStore().clearSearchParams();
     },
@@ -556,6 +581,18 @@ export default {
         return;
       }
 
+      this.adultStartDateRangeError = "";
+      if (
+        this.searchParams.adultStartDateFrom &&
+        this.searchParams.adultStartDateTo &&
+        new Date(this.searchParams.adultStartDateFrom) >
+          new Date(this.searchParams.adultStartDateTo)
+      ) {
+        this.adultStartDateRangeError =
+          "Adult Start Date From cannot be after Adult Start Date To.";
+        return;
+      }
+
       this.searchLoading = true;
       let sort = {};
       StudentProgramService.getStudentProgramsBySearchCriteria(
@@ -565,18 +602,15 @@ export default {
         this.itemsPerPage
       )
         .then((response) => {
-          console.log('Program search response:', response.data);
           if (response.data) {
             this.responseContent = response.data;
             this.searchResults = this.responseContent?.content?.map(item => this.transformProgramData(item)) || [];
-            console.log('Transformed search results with schools:', this.searchResults);
             this.totalElements = this.responseContent.totalElements || 0;
             this.totalPages = this.responseContent.totalPages || 0;
             this.searchMessage =
               this.responseContent.totalElements === 1
                 ? "1 result found"
                 : this.totalElements + " results found";
-            console.log('Total elements:', this.totalElements, 'Total pages:', this.totalPages, 'Search results length:', this.searchResults.length);
           }
         })
         .catch((error) => {
